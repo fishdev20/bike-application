75a9f613fc5b0d08d802b898f44891c2fe11ebd0
Minh Nguyen <minhfish20hp@gmail.com>
2022-08-02 22:33:36 +0300

    add average distance

--- frontend/src/components/ExpandData.js
+++ frontend/src/components/ExpandData.js
@@ -1,6 +1,8 @@
+/* eslint-disable default-case */
 import React, { useEffect, useState } from 'react'
 import axios from 'axios';
 import '../styles/stations.scss'
+import { useGlobal } from 'reactn';
 
 import { Col, Row } from 'antd';
 import EnvironmentTwoTone from '@ant-design/icons/EnvironmentTwoTone'
@@ -10,10 +12,12 @@ import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
 import { fetchAddress } from '../data/fetchData';
+import { sumBy } from 'lodash';
 
 export default function ExpandData({stationId}) {
     const [stationInfo,setStationInfo] = useState({})
     const [address, setAddress] = useState('')
+    const [journeys] = useGlobal('journeys')
 
     useEffect(() => {
         fetchStationInfo(stationId)
@@ -33,6 +37,21 @@ export default function ExpandData({stationId}) {
       const url = `https://api.digitransit.fi/geocoding/v1/reverse?point.lat=${position[0]}&point.lon=${position[1]}&size=1"`
       position[0] && fetchAddress(setAddress,url)
     },[position[0], position[1]])
+
+    const calculateAverageDistance = (kind) => {
+      let filteredInfo
+      switch(kind) {
+        case 'START':
+          filteredInfo = journeys?.filter(journey => journey.departureStationName === stationInfo.name)
+          break;
+        case 'END': 
+          filteredInfo = journeys?.filter(journey => journey.returnStationName === stationInfo.name)
+          break;
+      }
+      const averageDistance = sumBy(filteredInfo, (item) => {return item.distance}) / filteredInfo?.length
+      return averageDistance.toFixed(2)
+    }
+    
   return (
     <div>
         {!Boolean(stationInfo.name) ? 
@@ -41,6 +60,8 @@ export default function ExpandData({stationId}) {
           <Col md={12} xs={24}>
             <h4>- Total number of journeys starting from the station: <span style={{color: '#007ac9'}}>{stationInfo.journeysStart}</span> journeys</h4>
             <h4>- Total number of journeys ending at the station: <span style={{color: '#007ac9'}}>{stationInfo.journeysEnd}</span> journeys</h4>
+            <h4>- The average distance of a journey starting from the station: {calculateAverageDistance('START')} km</h4>
+            <h4>- The average distance of a journey ending at the station: {calculateAverageDistance('END')} km</h4>
           </Col>
           <Col md={12} xs={24}>
             <h4> <EnvironmentTwoTone /> Location </h4>
